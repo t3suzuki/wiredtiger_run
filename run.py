dcd2e0a59301a8ce51b451107b5d11f7bb783e4e
Tomoya Suzuki <t3suzuki@ucsd.edu>
2023-10-23 05:04:26 +0000

    up

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 import subprocess, os
 import textwrap
 
-ABT_PATH = "/home/tomoya-s/work/github/argobots/install"
+#ABT_PATH = "/home/tomoya-s/work/github/argobots/install"
+#ABT_PATH = "/home/tomoya-s/work/github/ppopp21-preemption-artifact/argobots/install"
+ABT_PATH = "/home/tomoya-s/mountpoint2/tomoya-s/argobots/install"
 MYLIB_PATH = "/home/tomoya-s/work/pthabt/newlib"
 
 def get_wtperf_cmd(op, n_th, cache_size, db_path):
@@ -16,7 +18,7 @@ def get_wtperf_cmd(op, n_th, cache_size, db_path):
         create = "false"
         
     conf_str = textwrap.dedent('''
-conn_config="cache_size={cache_size},direct_io=(data,checkpoint),eviction=(threads_min=12,threads_max=12),eviction_target=60,session_max={session_max}"
+conn_config="cache_size={cache_size},direct_io=(data,checkpoint),eviction=(threads_min=2,threads_max=2),eviction_target=80,session_max={session_max}"
 table_config="type=file,leaf_page_max=4k,internal_page_max=4k,checksum=on"
 icount={num}
 key_sz={key_size}
@@ -24,10 +26,10 @@ value_sz={val_size}
 report_interval=1
 run_time={duration}
 populate_threads=1
-verbose=1
+verbose=3
 create={create}
 threads=((count={n_th},reads=1))
-warmup=30
+warmup=60
 ''').format(session_max=n_th, create=create, n_th=n_th, num=num, key_size=key_size, val_size=val_size, duration=duration, cache_size=cache_size)
 
     cfg_filename = "wtperf.cfg"
@@ -43,7 +45,7 @@ def run(mode, op, n_core, n_th, cache_size):
     if mode == "abt":
         db_path = "/home/tomoya-s/mountpoint/tomoya-s/wt_abt250m"
     else:
-        db_path = "/home/tomoya-s/mountpoint/tomoya-s/wt_native250m"
+        db_path = "/home/tomoya-s/mountpoint2/tomoya-s/wt_native250m"
     
     if op == "set":
         print("We are modifying database {}. Are you Sure? (Y/N)".format(db_path))
@@ -56,7 +58,8 @@ def run(mode, op, n_core, n_th, cache_size):
         process = subprocess.run(mylib_build_cmd.split())
         my_env["LD_PRELOAD"] = MYLIB_PATH + "/mylib.so"
         my_env["LD_LIBRARY_PATH"] = ABT_PATH + "/lib"
-        my_env["ABT_THREAD_STACKSIZE"] = "65536"
+        #my_env["ABT_PREEMPTION_INTERVAL_USEC"] = "1000000"
+        #my_env["ABT_THREAD_STACKSIZE"] = "65536"
         #my_env["LIBDEBUG"] = MYLIB_PATH + "/debug.so"
         cmd = get_wtperf_cmd(op, n_th, cache_size, db_path)
     else:
@@ -70,14 +73,23 @@ def run(mode, op, n_core, n_th, cache_size):
 #run("abt", "set", 1, 1, 1024*1024)
 #run("natve", "set", 1, 1, 1024*1024)
 
-run("native", "get", 8, 256, "12G")
-#run("abt", "get", 8, 256, "12G")
+run("native", "get", 8, 64, "12G")
+#run("native", "get", 1, 32, "12G")
+#run("native", "get", 1, 1, "12G")
+#for i in range(0, 10):
+#    run("abt", "get", 8, 64, "12G")
+#run("abt", "get", 1, 32, "12G")
 
 # for n_core in [8,4,2,1]:
 #     for n_pth in [256,128,64,32,16]:#[16,32,64,128,256]:
-# #        run("abt", "get", n_core, n_pth, "12G")
+#         run("abt", "get", n_core, n_pth, "12G")
 #         run("native", "get", n_core, n_pth, "12G")
-    
+  
+# for n_core in [1,2,4,8]:
+#     for n_pth in [16,32,64,128,256]:
+#         run("abt", "get", n_core, n_pth, "12G")
+        
 # for n_core in [1,2,4,8]:
 #     for n_pth in [8,16,32,64,128]:
 #         run("native", "get", n_core, n_pth, "12G")
+
